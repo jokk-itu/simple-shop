bf2ada80e09cb0614e4addb142e4247db0a65b6a
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-07-11 17:38:46 +0200

    add: base types in domain driven design

---
+++ Api/Domain/Abstract/AggregateRoot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Api.Domain.Abstract;
+
+public abstract class AggregateRoot : Entity
+{
+  private readonly IList<Func<IDomainEvent>> _domainEvents = new List<Func<IDomainEvent>>();
+  public IReadOnlyCollection<Func<IDomainEvent>> DomainEvents => _domainEvents.AsReadOnly();
+
+  protected void RaiseEvent(Func<IDomainEvent> domainEventInvocation)
+  {
+    _domainEvents.Add(domainEventInvocation);
+  }
+}
---
+++ Api/Domain/Abstract/Entity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Api.Domain.Abstract;
+
+#pragma warning disable S4035 // The responsibility of equality comparison is immutable
+public abstract class Entity : IEquatable<Entity>
+#pragma warning restore S4035
+{
+  public int Id { get; private set; }
+
+  public bool IsTransient()
+  {
+    return Id == default;
+  }
+
+  public bool Equals(Entity? other)
+  {
+    return Equals((object?)other);
+  }
+
+  public override bool Equals(object? obj)
+  {
+    return obj is Entity entity
+           && entity.GetType() == GetType()
+           && !entity.IsTransient()
+           && !IsTransient()
+           && Id == entity.Id;
+  }
+
+  public override int GetHashCode()
+  {
+    return Id.GetHashCode();
+  }
+
+  public static bool operator ==(Entity left, Entity right)
+  {
+    return left.Equals(right);
+  }
+
+  public static bool operator !=(Entity left, Entity right)
+  {
+    return !left.Equals(right);
+  }
+}
