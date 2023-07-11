269723ff0aa21df63d1994f4ebc323dc2ac073fb
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-07-11 15:18:03 +0200

    add: unit of work

---
+++ Api/Infrastructure/Abstract/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Api.Infrastructure.Abstract;
+
+public interface IUnitOfWork
+{
+  Task Commit();
+}
---
+++ Api/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Api.Infrastructure.Abstract;
+
+namespace Api.Infrastructure;
+
+public class UnitOfWork : IUnitOfWork
+{
+  private readonly ShopContext _shopContext;
+
+  public UnitOfWork(ShopContext shopContext)
+  {
+    _shopContext = shopContext;
+  }
+
+  public Task Commit()
+  {
+    return _shopContext.SaveChangesAsync();
+  }
+}
