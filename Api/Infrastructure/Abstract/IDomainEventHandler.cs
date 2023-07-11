6e4177f7ca1d7ed9ae987fd7b5e715c637b5ebfa
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-07-11 17:04:53 +0200

    add: idomaineventhandler

---
+++ Api/Infrastructure/Abstract/IDomainEventHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Api.Domain.Abstract;
+
+namespace Api.Infrastructure.Abstract;
+
+public interface IDomainEventHandler<in TDomainEvent>
+  where TDomainEvent : IDomainEvent
+{
+    Task Handle(TDomainEvent domainEvent, CancellationToken cancellationToken);
+}
