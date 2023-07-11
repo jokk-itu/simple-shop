0fce7d0483a74f02f212725924e5a4d6685b756e
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2023-07-11 15:15:14 +0200

    add: shopcontext as dbcontext

---
+++ Api/Infrastructure/ShopContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Api.Infrastructure;
+
+public class ShopContext : DbContext
+{
+  public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+  { }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    base.OnModelCreating(modelBuilder);
+    modelBuilder.ApplyConfigurationsFromAssembly(typeof(ShopContext).Assembly);
+  }
+}
